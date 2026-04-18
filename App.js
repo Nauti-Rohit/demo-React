3dd6730a59967f06b7572ef8cedc350e00bee1fd
Nauti-Rohit <rohitnautiyal9@gmail.com>
2026-04-18 20:21:16 +0530

    Header component

--- App.js
+++ App.js
@@ -1,29 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "Learning React By Rohit Nautiyal"),
-  ),
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="header-logo" src="assets/logo/logo.png" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home </li>
+          <li>About Us </li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-const jsxHeading = <h1> This is my first lesson using JSX </h1>;
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
 
-//functional Component
-const HeadingComponent = () => (
-  <h1 className="heading">Hello! i am not Rohit Nautiyal</h1>
-);
-//component composition
-const HeadingComponent1 = () => (
-  <div id="container">
-    <HeadingComponent />
-    <h1 className="heading1">Hello! i am Rohit Nautiyal</h1>
-  </div>
-);
-
-const root = ReactDOM.createRoot(document.getElementById("parent"));
-root.render(<HeadingComponent1 />); // ✅ correct
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
