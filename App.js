8ae84f4fdfd1b7176d95805dc9be5010f8d92ad2
Nauti-Rohit <rohitnautiyal9@gmail.com>
2026-04-20 03:12:24 +0530

    header complete

--- App.js
+++ App.js
@@ -4,16 +4,21 @@ import ReactDOM from "react-dom/client";
 const Header = () => {
   return (
     <div className="header">
-      <div className="logo-container">
-        <img className="header-logo" src="assets/logo/logo.png" />
-      </div>
-      <div className="nav-items">
-        <ul>
-          <li>Home </li>
-          <li>About Us </li>
-          <li>Contact Us</li>
-          <li>Cart</li>
-        </ul>
+      <div className="header-container">
+        <div className="logo-container">
+          <img
+            className="header-logo"
+            src="https://marketplace.canva.com/EAGXsRURT9o/1/0/1600w/canva-yellow-and-brown-kitchen-food-logo-JrzZUwc_CLQ.jpg"
+          />
+        </div>
+        <div className="nav-items">
+          <ul>
+            <li>Home </li>
+            <li>About Us </li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
